8d4dbbf233fa0c9832288fcc1230fee28c515915
linse <linse@korb.linse.me>
2015-05-22 14:39:34 -0500

    print a curl command to talk to github - sketch as ajax call

--- commentserver.js
+++ commentserver.js
@@ -13,6 +13,7 @@ var server = https.createServer(options, function (req, res) {
   });
   req.on('end', function () {
     console.log('POSTed: ' + body + decodeURI(body) );
+    console.log(composeGist(decodeURI(body),counter++));
     // TODO contact github
     // we are on another port so we need to accept port 80's request
     res.setHeader('Access-Control-Allow-Origin', 'https://linse.me');
@@ -25,4 +26,23 @@ server.listen(8000);
 
 // Put a friendly message on the terminal
 console.log("Server running at http://127.0.0.1:8000/");
-console.log("🎂")
+console.log("🎂");
+var counter = 0;
+
+function composeGist(data,counter) {
+  return 'curl --user "linse" --data \'{"description":"From commentserver","public":"true","files":{"Comment'+counter+'.txt":{"content":"'+data+'"}}\' https://api.github.com/gists';
+}
+
+// TODO set this up to do what whe are duing via curl above
+function (e) {
+
+    $.ajax({
+      type: 'post',
+      url: 'https://linse.me:8000',
+      data: $('form').serialize(),
+      success: function () {
+        alert('form was submitted');
+      }
+    });
+
+}
