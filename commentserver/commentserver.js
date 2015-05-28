ba37e92c588b44b494eb49a414d16082bcbee1e1
linse <linse@korb.linse.me>
2015-05-28 17:43:12 -0500

    lost and found

--- commentserver/commentserver.js
+++ commentserver/commentserver.js
@@ -15,7 +15,8 @@ var server = https.createServer(options, function (req, res) {
   });
   req.on('end', function () {
     // build and post gist from POSTed form message
-    composeAndPostGist(body,counter++);
+    var formData = deserializeForm(body);
+    composeAndPostPR(formData);
     res.end("Thank you");
   });
 })
@@ -40,6 +41,46 @@ function patchGist(content) {
 
 function postGist(content) {
   sendGithubRequest('POST', '/gists', content);
+}
+
+// this is happening
+function composeAndPostPR(formData) {
+  console.log(formData);
+  // new branch - TODO what if it exists?
+  exec("cd "+options.local_repo+" && git checkout -b "+formData.name, puts); 
+  // add comment
+  exec("cat \""+formData.name+" posted a message "+formData.message
+     +"\n\" >> "+options.local_repo+"/content/"+formData.file.replace("html","md"), puts); // TODO optionize
+  //var data = '{"title":"Amazing new feature","body":"please pull this in!","head":"linse:master","base":"master"}';
+  //sendGithubRequest('POST', '/repos/linse/zeckernews/pulls', data);
+  exec("cd "+options.local_repo+" && git checkout master", puts); 
+}
+
+function puts(error, stdout, stderr) { sys.puts(stdout) }
+function devnull(error, stdout, stderr) { }
+
+// TODO branch with users name
+// TODO what happens if branch already exists? (middle statment fail) - generate branchnames with increasing number or nonce?
+function newBranch(name) {
+  exec("cd "+options.local_repo+" && git checkout -b "+name, puts); 
+  // needs auth exec("git push origin "+name, puts);
+}
+
+function appendTo(comment, filename) {
+  exec("cat "+comment+" >> "+options.local_repo+"/"+filename, puts);
+}
+
+function commit() {
+  exec("cd "+options.local_repo+" && git commit -a -m new comment", puts); 
+}
+
+function getBranches() {
+  sendGithubRequest('GET','/repos/linse/zeckernews/git/refs/heads','');
+}
+
+function pushBranch(branchname,sha) {
+  var data = '{"ref":"/refs/heads/'+branchname+'","sha":"'+sha+'"}';
+  sendGithubRequest('GET','/repos/linse/zeckernews/git/refs',data);
 }
 
 function sendGithubRequest(method, path, content) {
@@ -68,4 +109,19 @@ function deserializeMessage(string) {
   var message = parts[1].replace(/\+/g," ");
   // decode as last step - don't remove pluses from form input
   return decodeURIComponent(message);
+}
+
+function deserializeForm(string) {
+  var parts = string.split("&");
+  if (parts.length != 3) {
+    throw "need request with filename, username, message from form!";
+  }
+  // throw away part before =, replace pluses which encode space
+  parts = parts.map( function (p) {
+    return p.split("=")[1]
+  } );
+  // decode as last step - so we don't remove pluses from form input
+  return { 'file': parts[0],
+           'name': decodeURIComponent(parts[1].replace(/\+/g," ")),
+        'message': decodeURIComponent(parts[2].replace(/\+/g," ")) };
 }
