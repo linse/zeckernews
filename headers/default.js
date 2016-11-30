a19db283e24ec92f9112b3bde15603a5d3868ea0
linse <linse@korb.linse.me>
2016-11-30 16:37:47 -0600

    Split up the header JS into work and play.

--- headers/default.js
+++ headers/default.js
@@ -2,31 +2,37 @@
 <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
 <link href="https://linse.me/feed.xml" rel="alternate" type="application/rss+xml" title="Posts RSS" />
 <script type="text/javascript">
-  window.onload = function() {
+  $(window).on('load',init);
+//  $(window).on('resize',init);
+//  $(window).on('beforeunload',init);
+
+  function init() {
     $('.slippy-nav').addClass('original').clone().insertAfter('.slippy-nav')
                     .addClass('cloned').css('position','fixed').css('top','0').css('margin-top','0').css('z-index','500')
                     .removeClass('original').hide();
-    var scrollIntervalID = setInterval(stickIt.bind(null, $('.original'), $('.cloned')), 10);
+    var scrollIntervalID = setInterval(stickNav.bind(null, $('.original'), $('.cloned')), 10);
   }
 
-  function stickNav(slippynavPosition) {
-    console.log(slippynavPosition);
-  }
-
-  function stickIt(original, cloned) {
-    var orgElementTop = original.offset().top;               
-  
-    if ($(window).scrollTop() >= orgElementTop) { // scrolled past the original position
-      // cloned should always have same geometry as original
-      var leftOriginal = original.offset().left;
-      var widthOriginal = original.css('width');
-      // now only show the cloned, sticky element.
-      cloned.css('left',leftOriginal+'px').css('top',0).css('width',widthOriginal).show();
-      original.css('visibility','hidden');
-    } else { // not scrolled past the slippy-nav
-      cloned.hide();
-      // only show the original slippy-nav
-      original.css('visibility','visible');
+  function stickNav(original, cloned) {
+    var scrollPosition = $(window).scrollTop();
+    var originalPosition = original.offset().top;
+    if (scrollPosition >= originalPosition) {
+      onlyShowCloned(original, cloned);
+    } else {
+      onlyShowOriginal(original, cloned);
     }
+  }
+
+  function onlyShowCloned(original, cloned) {
+    // cloned should have same geometry as original
+    var left = original.offset().left;
+    var width = original.css('width');
+    cloned.css('left',left+'px').css('top',0).css('width',width).show();
+    original.css('visibility','hidden');
+  }
+
+  function onlyShowOriginal(original, cloned) {
+    cloned.hide();
+    original.css('visibility','visible');
   }
 </script>
--- headers/rainbow.js
+++ headers/rainbow.js
@@ -1,61 +1,79 @@
 <script type="text/javascript">
+  $(window).on('load',init);
+//  $(window).on('resize',init);
+  $(window).on('beforeunload',init);
 
-//function rainbow(c, color, numPixels) {
-//    color = color * 240 / (numPixels - Math.max(c.width,c.height)); // could be 255
-//    return 'hsl(' + color + ',100%,75%)';
-//}
-//
-//function numPixels(canvas, pixelWidth) {
-//    return Math.ceil((canvas.width * canvas.height) / (pixelWidth * pixelWidth)) 
-//          + (Math.max(canvas.width,canvas.height));
-//}
-//
-//function fillPixel(c, color, w) {
-//    var ctx = c.getContext("2d")
-//    if (row*w > c.height) {
-//            col++;
-//            row = 0;
-//    }
-//    ctx.beginPath();
-//    ctx.rect(col*w, row*w, w, w);
-//    ctx.fillStyle = rainbow(c, color, numPixels(c,w));
-//    ctx.fill();
-//    row++;
-//}
-//
-//// filling the canvas black as a test
-//function fillCanvas(c) {
-//    var ctx = c.getContext("2d")
-//    ctx.fillRect(0, 0, c.width, c.height);
-//}
-//
-//function init() {
-//    var c = document.getElementById("header")
-//    resizeCanvas(c);
-//    fillCanvas(c);
-//
-//    var pixelWidth = 20;
-//    var colors = [];
-//    for (var i = 1; i <= numPixels(c,pixelWidth); i++) {
-//      colors.push(i);
-//    }
-//
-//    col = 0; 
-//    row = 0;
-//
-//    //shuffle(colors);
-//    colors.forEach(function(color) {
-//      fillPixel(c,color,pixelWidth);
-//    })
-//    //setTimeout(init, 100);
-//    window.scrollTo(0, 170);
-////    stickNav();
-//}
-//
-//$(function() {
-//  init();
-//});
-//
-//
-//window.onresize = init;
+  function init() {
+    window.scrollTo(0, 0);
+    window.scrollTo(0, 170);
+    for (var i = 1; i <= 500; i++) {
+        (function(index) {
+            setTimeout(function() { drawPixels(index); }, i * 100);
+        })(i);
+    }
+  }
+
+  function drawPixels(i) {
+    var canvas = $('#header').get(0)
+    resizeCanvas(canvas);
+
+    // fillCanvas(c);
+  
+    var pixelWidth = 10;
+    var colors = [];
+    for (var i = 1; i <= numPixels(canvas, pixelWidth); i++) {
+      colors.push(i);
+    }
+  
+    col = 0; 
+    row = 0;
+  
+    shuffle(colors);
+    colors.forEach(function(color) {
+      fillPixel(canvas, color, pixelWidth);
+    })
+  }
+
+  function resizeCanvas(canvas) {
+    canvas.width = $(window).width()
+  }
+
+  // filling the canvas black as a test
+  function fillCanvas(canvas) {
+    var ctx = canvas.getContext("2d")
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+  }
+
+  function rainbow(c, color, numPixels) {
+    color = color * 240 / (numPixels - Math.max(c.width,c.height)); // could be 255
+    return 'hsl(' + color + ',100%,75%)';
+  }
+
+  function numPixels(canvas, pixelWidth) {
+    return Math.ceil((canvas.width * canvas.height) / (pixelWidth * pixelWidth)) 
+          + (Math.max(canvas.width, canvas.height));
+  }
+
+  function fillPixel(c, color, w) {
+    var ctx = c.getContext("2d")
+    if (row*w > c.height) {
+      col++;
+      row = 0;
+    }
+    ctx.beginPath();
+    ctx.rect(col*w, row*w, w, w);
+    ctx.fillStyle = rainbow(c, color, numPixels(c,w));
+    ctx.fill();
+    row++;
+  }
+
+  function shuffle(a) {
+      var j, x, i;
+      for (i = a.length; i; i--) {
+          j = Math.floor(Math.random() * i);
+          x = a[i - 1];
+          a[i - 1] = a[j];
+          a[j] = x;
+      }
+  }
 </script>
